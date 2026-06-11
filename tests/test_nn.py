1932408fc18a4e5f564ec7f99c1e8061c7055425
lucidrains <lucidrains@gmail.com>
2026-06-11 10:25:35 -0700

    residual

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torch_einops_utils.nn import Sequential, Lambda, Identity
+from torch_einops_utils.nn import Sequential, Lambda, Identity, Residual
 
 def test_sequential():
     # Test that it filters out None
@@ -26,3 +26,18 @@ def test_identity():
     ident = Identity()
     x = torch.tensor([1., 2., 3.])
     assert torch.allclose(ident(x), x)
+
+def test_residual():
+    fn = lambda x: x * 2
+    res = Residual(fn)
+    x = torch.tensor([1., 2., 3.])
+    assert torch.allclose(res(x), torch.tensor([3., 6., 9.]))
+
+    def fn_tuple(x):
+        return x * 2, x * 3, dict(a = x * 4)
+
+    res_tuple = Residual(fn_tuple)
+    out1, out2, out3 = res_tuple(x)
+    assert torch.allclose(out1, torch.tensor([3., 6., 9.]))
+    assert torch.allclose(out2, torch.tensor([3., 6., 9.]))
+    assert torch.allclose(out3['a'], torch.tensor([4., 8., 12.]))
--- torch_einops_utils/nn.py
+++ torch_einops_utils/nn.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from typing import Callable
 
 from torch.nn import Module, Sequential as PyTorchSequential
-from torch_einops_utils.torch_einops_utils import exists
+from torch_einops_utils.torch_einops_utils import exists, tree_flatten_with_inverse
 
 def Sequential(*modules):
     return PyTorchSequential(*filter(exists, modules))
@@ -18,3 +18,14 @@ class Lambda(Module):
 
     def forward(self, t, *args, **kwargs):
         return self.fn(t, *args, **kwargs)
+
+class Residual(Module):
+    def __init__(self, fn: Callable):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, *args, **kwargs):
+        out = self.fn(x, *args, **kwargs)
+
+        (first, *rest), inverse = tree_flatten_with_inverse(out)
+        return inverse((first + x, *rest))
